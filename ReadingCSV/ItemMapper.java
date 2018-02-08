6ed767a61626884ae14a174e61ac71bb6efb80dc
Muhammad Shafay Amjad <shafaypro@users.noreply.github.com>
2018-02-08 14:14:03 +0500

    Update ItemMapper.java

--- ReadingCSV/ItemMapper.java
+++ ReadingCSV/ItemMapper.java
@@ -14,8 +14,10 @@ import org.apache.flink.util.Collector;  // for the usage of the collector. 1
 public class ItemMapper implements FlatMapFunction<Item,String>{
 @Override
 public void flatMap(Item val , Collector<String> out) throws Exception
+	// The function has the Item <-- as an input object(datatype) and a collection output which is "String based"
 	{ //System.out.println(val.getItemid()+ " == ")
 	out.collect(val.getItem_name());  // this gets the collection item name 
+	// one by one the collection is passed (a value from the varaiable of the Item Object (function get ITemname)
 	}
 
 }

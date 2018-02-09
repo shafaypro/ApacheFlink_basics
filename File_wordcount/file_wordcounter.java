626c1a78c93641ecd7411b9435c130488aa821d0
Muhammad Shafay Amjad <shafaypro@users.noreply.github.com>
2018-02-09 17:38:19 +0500

    Add files via upload
    Adding in the absloute path for the data writing,

--- File_wordcount/file_wordcounter.java
+++ File_wordcount/file_wordcounter.java
@@ -18,16 +18,17 @@ public class file_wordcounter {
 		Date date = new Date();
 		System.out.println(date);
 		ExecutionEnvironment File_Environment = ExecutionEnvironment.getExecutionEnvironment(); // importing the execution default envirenoment
+		File_Environment.setParallelism(1);
 		DataSet<Tuple2<String, Integer>> counts  = File_Environment
-		.readTextFile("book.txt").
+		.readTextFile("C:\\Users\\shafay.amjad\\Desktop\\APACHE_WORKSPACE\\quickstart\\src\\main\\java\\File_wordcount\\book.txt").
 		flatMap(new wordcountmapper())
 		.groupBy(0)
 		.sum(1); // reading the text file using the constant envirenoment.
 		
 		// Working below
 		counts.writeAsCsv("C:\\Users\\shafay.amjad\\Desktop\\APACHE_WORKSPACE\\quickstart\\src\\main\\java\\File_wordcount\\outputfrom_wordcount.csv", "\n", " ");
-			
 		System.out.println(date);
+		counts.print();
 	}
 	
 	
